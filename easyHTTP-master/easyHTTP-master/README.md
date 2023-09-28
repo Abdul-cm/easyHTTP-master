468016e7bb6107779cc8cc2814bee871c4fecabe
Abdul-cm <84390908+Abdul-cm@users.noreply.github.com>
2023-09-28 15:55:25 +0100

    Add files via upload

---
+++ easyHTTP-master/easyHTTP-master/README.md
@@ -0,0 +1,12 @@
+# easyHTTP
+
+## About
+
+This is a simple HTTP library made with the Fetch API and ES6 OOP
+
+## Usage
+
+To start using easyHttp you need to include it to your project and then instantiate the easyHTTP object:
+const http = new easyHTTP();
+
+Read app.js for examples
---
+++ easyHTTP-master/easyHTTP-master/easyhttp.js
@@ -0,0 +1,87 @@
+/**
+ * EasyHTTP Library
+ * Library for making HTTP requests
+ * Inspired by Brad Traversy
+ *
+ * @version 3.0.0
+ * @author  Thomas Rogers
+ * @license MIT
+ *
+ **/
+
+class easyHTTP {
+	//This function is to format the data in the correct content type
+	async formatData(contentType, data){
+		let formattedData;
+		switch(contentType){
+			case "application/json":
+				formattedData = await data.json();
+				break;
+			case "text/xml":
+				formattedData = await data.text();
+				let parser = new DOMParser();
+				formattedData = parser.parseFromString(formattedData, contentType);
+				break;
+			case "text/plain":
+				formattedData = await data.text();
+				break;
+			default:
+		}
+		
+		return formattedData;
+	}
+	
+  	// Make an HTTP GET Request
+  	async get(url, contentType = "application/json") {
+    	const response = await fetch(url, {
+      		method: 'GET',
+      		headers: {
+        		'Content-type': contentType
+      		}
+    	});
+    	
+    	const formatResponse = this.formatData(contentType, response);
+    	return formatResponse;
+  	}
+
+  	// Make an HTTP POST Request
+  	async post(url, data, contentType = "application/json") {
+    	const response = await fetch(url, {
+      		method: 'POST',
+      		headers: {
+        		'Content-type': contentType
+      		},
+      		body: JSON.stringify(data)
+    	});
+
+    	const formatResponse = this.formatData(contentType, response);
+    	return formatResponse;
+  	}
+
+    // Make an HTTP PUT Request
+  	async put(url, data, contentType = "application/json") {
+	    const response = await fetch(url, {
+	      method: 'PUT',
+	      headers: {
+	        'Content-type': contentType
+	      },
+	      body: JSON.stringify(data)
+	    });
+	
+	    const formatResponse = this.formatData(contentType, response);
+	    return formatResponse;
+	}
+
+   // Make an HTTP DELETE Request
+   async delete(url, contentType = "application/json") {
+      const response = await fetch(url, {
+	      method: 'DELETE',
+	      headers: {
+	        'Content-type': contentType
+	      }
+	  });
+
+	  const formatResponse = this.formatData(contentType, response);
+	  return formatResponse;
+   }
+}
